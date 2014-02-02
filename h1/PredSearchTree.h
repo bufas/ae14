a5435bc228d4d749a3ebe221787436ab8c72dccf
Bufas <bufas7@gmail.com>
2014-02-02 23:56:32 +0100

    Search tree interface

---
+++ h1/PredSearchTree.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+class PredSearchTree {
+
+public:
+
+	PredSearchTree(std::vector<int> v) explicit {}
+	virtual ~PredSearchTree() {}
+
+	virtual int pred(int x) = 0;
+
+};
