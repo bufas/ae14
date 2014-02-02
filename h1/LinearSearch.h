db2a5f859e157ada5b052b8a6910feab13fd2ab5
Bufas <bufas7@gmail.com>
2014-02-03 00:16:43 +0100

    Linear pred search

---
+++ h1/LinearSearch.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+
+class LinearSearch : public PredSearchTree {
+
+private:
+	std::vector<int> elems;
+
+public:
+
+	LinearSearch(std::vector<int> v) : elems(v) {
+		std::sort(elems.begin(), elems.begin());
+	}
+
+	virtual int pred(int x) {
+		int pred = -1;
+		for (auto it = elems.begin(); it != elems.end(); ++it) {
+			if (*it == x) return pred;
+			pred = *it;
+		}
+		return -1;
+	}
+
+};
