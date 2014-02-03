9841b8e03bf46843971d343c1d9193932818fcea
Bufas <bufas7@gmail.com>
2014-02-04 00:12:16 +0100

    Correct some errors, still buggy

--- h1/BFSBinarySearch.h
+++ h1/BFSBinarySearch.h
@@ -6,44 +6,57 @@
 class BFSBinarySearch : public PredSearchTree {
 
 private:
-    std::vector<int> elems; // TODO this should be deleted
-    std::vector<int> tree; // TODO this should be std::array pointer instead
+    std::vector<int> elems; // TODO this is just used for startup
+    std::vector<int> tree;
 
     // We need to add a little extra because we index from 0
     int child_left(int idx) { return idx * 2 + 1; }
     int child_right(int idx) { return idx * 2 + 2; }
-    int parent(int idx) { return (idx + 1) / 2; }
+    int parent(int idx) { return (idx - 1) / 2; }
 
     void make_bfs_layout(const std::vector<int> &v, int s, int e, int idx) {
+        if (s > e) return;
+
         int mid = (e + s) / 2;
 
-        tree[idx] = mid;
+        tree[idx] = v[mid];
 
-        make_bfs_layout(s, mid - 1, child_left(idx));
-        make_bfs_layout(mid + 1, e, child_right(idx));
+        make_bfs_layout(v, s,     mid-1, child_left(idx));
+        make_bfs_layout(v, mid+1, e,     child_right(idx));
     }
 
 public:
 
-    BFSBinarySearch(std::vector<int> v) : elems(v) {
+    BFSBinarySearch(const std::vector<int> &v) : elems(v), tree(v) {
         std::sort(elems.begin(), elems.end());
-        make_bfs_layout(v, 0, elems.size(), 0);
+        make_bfs_layout(elems, 0, elems.size()-1, 0);
     }
 
-    /**
-     * This is most likely wrong
-     */
-    virtual int pred(int x) {
-        int idx = 0;
+    int pred(int x) {
+        // Print the tree
+        std::cout << "Printing the tree" << std::endl;
+        for (auto it = tree.begin(); it != tree.end(); ++it) {
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
+
+        if (tree.empty()) return -1;
+
+        int idx = 0; // Set index to root
+        bool only_left = true;
 
         while (idx < tree.size()) {
             if (tree[idx] == x) return x;
-            
-            if (tree[idx] < x) idx = child_right(idx);
-            else idx = child_left(idx);
+
+            if (tree[idx] < x) {
+                only_left = false;
+                idx = child_right(idx);
+            } else {
+                idx = child_left(idx);
+            }
         }
 
-        return tree[parent(idx)];
+        return (only_left) ? -1 : tree[parent(idx)];
     }
 
 };
