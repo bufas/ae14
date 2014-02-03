7d5f2e5d3d3079aade14a4044132a7c5e4e72b6d
Bufas <bufas7@gmail.com>
2014-02-03 13:13:29 +0100

    Traditional binary search

---
+++ h1/SortedBinarySearch.h
@@ -0,0 +1,31 @@
+
+class SortedBinarySearch : public PredSearchTree {
+
+private:
+    std::vector<int> elems;
+
+public:
+
+    LinearSearch(std::vector<int> v) : elems(v) {
+        std::sort(elems.begin(), elems.begin());
+    }
+
+    /**
+     * This is most likely wrong
+     */
+    virtual int pred(int x) {
+        int s = 0;
+        int e = elems.size();
+
+        while (s < e) {
+            int mid = (e + s) / 2;
+            if (elems[mid] == x) return x;
+            
+            if (elems[mid] < x) s = mid + 1;
+            else e = mid - 1;
+        }
+
+        return elems[s];
+    }
+
+};
