8da4ffef746c58cf95e875c5ba21c3d5c513e505
Bufas <bufas7@gmail.com>
2014-02-03 13:49:03 +0100

    BFS layout binary search tree

---
+++ h1/BFSBinarySearch.h
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+
+class BFSBinarySearch : public PredSearchTree {
+
+private:
+    std::vector<int> elems; // TODO this should be deleted
+    std::vector<int> tree; // TODO this should be std::array pointer instead
+
+    // We need to add a little extra because we index from 0
+    int child_left(int idx) { return idx * 2 + 1; }
+    int child_right(int idx) { return idx * 2 + 2; }
+    int parent(int idx) { return (idx + 1) / 2; }
+
+    void make_dfs_layout(const std::vector<int> &v, int s, int e, int idx) {
+        int mid = (e + s) / 2;
+
+        tree[idx] = mid;
+
+        make_dfs_layout(s, mid - 1, child_left(idx));
+        make_dfs_layout(mid + 1, e, child_right(idx));
+    }
+
+public:
+
+    LinearSearch(std::vector<int> v) : elems(v) {
+        std::sort(elems.begin(), elems.end());
+        make_dfs_layout(v, 0, elems.size(), 0);
+    }
+
+    /**
+     * This is most likely wrong
+     */
+    virtual int pred(int x) {
+        int idx = 0;
+
+        while (idx < tree.size()) {
+            if (tree[idx] == x) return x;
+            
+            if (tree[idx] < x) idx = child_right(idx);
+            else idx = child_left(idx);
+        }
+
+        return tree[parent(idx)];
+    }
+
+};
