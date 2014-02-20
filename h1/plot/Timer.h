421e43fd80fa1d736f7b63fd68567d478444f110
Mathias Bak <bufas7@gmail.com>
2014-02-20 22:29:52 +0100

    Remove unused member functions

--- h1/plot/Timer.h
+++ h1/plot/Timer.h
@@ -5,8 +5,6 @@ public:
     void start();
     void stop();
 
-    long get_seconds()      { return get_microseconds() / 1000000; }
-    long get_milliseconds() { return get_microseconds() / 1000; }
     long get_microseconds() {
         long secs = after.tv_sec - before.tv_sec;
         long usecs = after.tv_usec - before.tv_usec;
