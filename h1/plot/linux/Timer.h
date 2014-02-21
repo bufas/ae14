b7b4fd09a22171518372b34c075b32c63a0ba532
Mathias Bak <bufas7@gmail.com>
2014-02-21 17:17:48 +0100

    Upgrade timer to handle arbitrary many counters
    The results returned for cache misses seems to be incorrect.

--- h1/plot/linux/Timer.h
+++ h1/plot/linux/Timer.h
@@ -1,4 +1,8 @@
 #include <sys/time.h>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cerrno>
 
 // This is for perf_event_open()
 #include <string.h>
@@ -18,47 +22,53 @@ public:
     void stop();
 
     long get_microseconds() {
-        long secs = after.tv_sec - before.tv_sec;
+        long secs  = after.tv_sec  - before.tv_sec;
         long usecs = after.tv_usec - before.tv_usec;
         return (secs * 1000000) + usecs;
     }
 
     long long get_branch_misses() {
-        return read_perf_event_open_value(fd_hw, PERF_COUNT_HW_BRANCH_MISSES);
+        return read_perf_event_open_value("branch", PERF_COUNT_HW_BRANCH_MISSES);
     }
 
     long long get_cache_misses() {
-        return read_perf_event_open_value(fd_hw, PERF_COUNT_HW_CACHE_MISSES);
+        return read_perf_event_open_value("branch", PERF_COUNT_HW_CACHE_MISSES);
     }
 
 
 private:
-    long long read_perf_event_open_value(const int &fd, const unsigned long config);
+    long long read_perf_event_open_value(const std::string &group, const unsigned long config);
 
-    void init_perf_event_open(long &fd, const unsigned int type, const unsigned long config);
+    void init_perf_event_open(const std::string &group, const unsigned int type, const unsigned long config);
 
     struct timeval before;
     struct timeval after;
 
     // File descriptors for various counters
-    long fd_hw;
-    std::vector<unsigned long> configs;
-    std::vector<long> open_handles;
+    std::vector<int> open_handles;
+
+    std::unordered_map<std::string, int> leaders;
+    std::unordered_map<std::string, std::vector<unsigned long>> configs;
 };
 
-Timer::Timer() : fd_hw(-1) {
-    init_perf_event_open(fd_hw, PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES);
-    init_perf_event_open(fd_hw, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
+Timer::Timer() {
+    // init_perf_event_open("branch", PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS);
+    init_perf_event_open("branch", PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES);
+
+    // init_perf_event_open("cache", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES);
+    init_perf_event_open("branch", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
 }
 
 Timer::~Timer() {
-    for (auto i = open_handles.begin(); i < open_handles.end(); ++i) close(*i);
+    for (auto i = open_handles.begin(); i != open_handles.end(); ++i) close(*i);
 }
 
 void Timer::start() {
     // Reset all counters and start them
-    ioctl(fd_hw, PERF_EVENT_IOC_RESET, 0); 
-    ioctl(fd_hw, PERF_EVENT_IOC_ENABLE, 0);
+    for (auto i = leaders.begin(); i != leaders.end(); ++i) {
+        ioctl(i->second, PERF_EVENT_IOC_RESET, 0); 
+        ioctl(i->second, PERF_EVENT_IOC_ENABLE, 0);
+    }
 
     // Start timer
     gettimeofday(&before, nullptr);
@@ -69,33 +79,42 @@ void Timer::stop() {
     gettimeofday(&after, nullptr);
 
     // Stop the counters
-    ioctl(fd_hw, PERF_EVENT_IOC_DISABLE, 0);
+    for (auto i = leaders.begin(); i != leaders.end(); ++i) {
+        ioctl(i->second, PERF_EVENT_IOC_DISABLE, 0);
+    }
 }
 
-long long Timer::read_perf_event_open_value(const int &fd, const unsigned long config) {
-    // Read from the file descriptor
-    long long results[1 + configs.size()];
-    int res = read(fd, &results, sizeof(results));
+long long Timer::read_perf_event_open_value(const std::string &group, const unsigned long config) {
+    // Check if the group exists
+    if (leaders.count(group) == 0) {
+        std::cout << "Error! Reading a value from a non existant group ("<<group<<")\n" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-    std::cout << "Printing results [";
-    for (int i = 0; i < 1 + configs.size(); i++) std::cout << " " << results[i];
-    std::cout << " ]" << std::endl;
+    // Get the leader of the group (file descriptor)
+    int leader = leaders.find(group)->second;
+
+    // Read from the leader
+    long long results[configs[group].size() + 1];
+    int ret = read(leader, &results, sizeof(results));
+
+    // std::cout << "Printing results [";
+    // for (int i = 0; i < configs[group].size() + 1; i++) std::cout << " " << results[i];
+    // std::cout << " ]" << std::endl;
 
     // Check for errors while reading
-    if (res < 0) {
-        fprintf(stderr, "Error reading perf_event_open value\n");
+    if (ret < 0) {
+        fprintf(stderr, "Error reading perf_event_open values\n");
         exit(EXIT_FAILURE);
     }
 
     // Return the requested value
-    for (int i = 0; i < configs.size(); ++i) {
-        if (configs[i] == config) {
-            return results[i+1];
+    auto v = configs[group];
+    for (int i = 0; i < v.size(); ++i) {
+        if (v[i] == config) {
+            return results[i + 1];
         }
     }
-
-    std::cout << "ERROR! The requested value was not found." << std::endl;
-    return -1000000000;
 }
 
 
@@ -106,42 +125,45 @@ long long Timer::read_perf_event_open_value(const int &fd, const unsigned long c
  *
  * Very old, much ANSI-C, WOW, such compatible
  */
-void Timer::init_perf_event_open(long &fd, const unsigned int type, const unsigned long config) {
+void Timer::init_perf_event_open(const std::string &group, const unsigned int type, const unsigned long config) {
     // Create the struct which defines what should be measured
     struct perf_event_attr pe;
     memset(&pe, 0, sizeof(struct perf_event_attr));
-    pe.size   = sizeof(struct perf_event_attr);
-    pe.type   = type;
-    pe.config = config;
-
-    // This should be the deafult values, no need to set
+    pe.size           = sizeof(struct perf_event_attr);
+    pe.type           = type;
+    pe.config         = config;
     pe.exclude_kernel = 1;
     pe.exclude_hv     = 1;
 
     // Use the mysterious function to get a file descriptor for the data file
-    if (fd == -1) {
+    int new_fd;
+    auto it_group_leader = leaders.find(group);
+    if (it_group_leader == leaders.end()) {
+        // Create a leader
         pe.disabled    = 1;
         pe.read_format = PERF_FORMAT_GROUP;
-        fd = perf_event_open(&pe, 0, -1, -1, 0);
-        if (fd == -1) {
+
+        new_fd = perf_event_open(&pe, 0, -1, -1, 0);
+        if (new_fd == -1) {
             fprintf(stderr, "Error opening leader %llx\n", pe.config);
             exit(EXIT_FAILURE);
         }
-        std::cout << "fd == " << fd << std::endl;
-        open_handles.push_back(fd);
+
+        // Update the hardware leader
+        leaders[group] = new_fd;
     }
     else {
-        pe.disabled    = 0;
-        int ret = perf_event_open(&pe, 0, -1, fd, 0);
-        if (ret == -1) {
-            fprintf(stderr, "Error connecting to leader %llx\n", pe.config);
+        // Create a slave
+        pe.disabled = 0; // Slaves are still disabled when leader is disabled
+        new_fd = perf_event_open(&pe, 0, -1, it_group_leader->second, 0);
+        if (new_fd == -1) {
+            fprintf(stderr, "Error connecting to leader %llx. Errno: %d\n", pe.config, errno);
             exit(EXIT_FAILURE);
         }
-        std::cout << "ret == " << ret << std::endl;
-        open_handles.push_back(ret);
     }
 
-    configs.push_back(config); // TODO this only accomodates type = HARDWARE
+    open_handles.push_back(new_fd);   // Remember the file descriptors so we can close them again
+    configs[group].push_back(config); // Remember the index of the configs so they can be read
 }
 
 /**
