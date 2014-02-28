22c70a2d3d7742d9d801e049b12af057e02af595
Bufas <bufas7@gmail.com>
2014-02-28 15:59:17 +0100

    Fix ratio plots

--- h1/plot/gen_gnuplot_input.py
+++ h1/plot/gen_gnuplot_input.py
@@ -60,8 +60,12 @@ def make_plot(output, impl, perf, block, start=1, end=0):
     print 'plot \\'
     for i in impl:
         endl = '' if (i == impl[-1]) else ', \\'
-        print '"res/%s.%s.bench.data" %s using (log($1)/log(2)):($%d/$2):($%d/$2):($%d/$2) title "%s" with errorlines%s' \
-        % (i, perf, every, block, block+1, block+2, titles[i], endl)
+        if block == 12:
+            print '"res/%s.%s.bench.data" %s using (log($1)/log(2)):(100-$%d):(100-$%d):(100-$%d) title "%s" with errorlines%s' \
+                % (i, perf, every, block, block+1, block+2, titles[i], endl)
+        else:
+            print '"res/%s.%s.bench.data" %s using (log($1)/log(2)):($%d/$2):($%d/$2):($%d/$2) title "%s" with errorlines%s' \
+                % (i, perf, every, block, block+1, block+2, titles[i], endl)
 
     # Print som blank space so the file becomes readable
     print
@@ -75,7 +79,7 @@ print
 # Plot small instances to test L1 cache misses
 #===================================================#
 
-# Test cache performance around cache sizes
+# Plot cache performance around cache sizes
 make_plot('small_l1cache_time.png',  impl['all'], 'L1_CACHE', blocks['time'],  8, 14)
 make_plot('medium_llcache_time.png', impl['all'], 'LL_CACHE', blocks['time'], 14, 19)
 make_plot('large_nocache_time.png',  impl['all'], 'LL_CACHE', blocks['time'], 18)
@@ -83,15 +87,19 @@ make_plot('large_nocache_time.png',  impl['all'], 'LL_CACHE', blocks['time'], 18
 make_plot('small_l1cache.png',       impl['all'], 'L1_CACHE', blocks['miss'],  8, 14)
 make_plot('medium_llcache.png',      impl['all'], 'LL_CACHE', blocks['miss'], 14, 19)
 
-# Plot all for every performance criteria
-make_plot('all/time.png',          impl['all'], 'LL_CACHE', blocks['time'])
+# Plot all, implicit, and explicit for every performance criteria
+make_plot('all/time.png',      impl['all'],      'LL_CACHE', blocks['time'])
+make_plot('implicit/time.png', impl['implicit'], 'LL_CACHE', blocks['time'])
+make_plot('explicit/time.png', impl['explicit'], 'LL_CACHE', blocks['time'])
 
-make_plot('all/perf_bpu.png',      impl['all'], 'BPU',      blocks['miss'])
-make_plot('all/perf_l1cache.png',  impl['all'], 'L1_CACHE', blocks['miss'])
-make_plot('all/perf_llcache.png',  impl['all'], 'LL_CACHE', blocks['miss'])
-make_plot('all/perf_tlb.png',      impl['all'], 'LL_CACHE', blocks['miss'])
+for b in ['miss', 'access', 'ratio']:
+    for k in perf_labels:
+        make_plot('all/%s_%s.png' % (b, k), impl['all'], k, blocks[b])
+        make_plot('implicit/%s_%s.png' % (b, k), impl['implicit'], k, blocks[b])
+        make_plot('explicit/%s_%s.png' % (b, k), impl['explicit'], k, blocks[b])
 
-make_plot('all/ratio_bpu.png',     impl['all'], 'BPU',      blocks['ratio'])
-make_plot('all/ratio_l1cache.png', impl['all'], 'L1_CACHE', blocks['ratio'])
-make_plot('all/ratio_llcache.png', impl['all'], 'LL_CACHE', blocks['ratio'])
-make_plot('all/ratio_tlb.png',     impl['all'], 'LL_CACHE', blocks['ratio'])
+# Plot explicit int vs explicit ptr (only BFS and DFS)
+explicit_int_ptr = impl['explicit_int'] + impl['explicit_ptr'] # make union
+make_plot('explicit_intVSptr_time.png', explicit_int_ptr, 'LL_CACHE', blocks['time'])
+for k in perf_labels:
+    make_plot('explicit_intVSptr_%s.png' % k, explicit_int_ptr, k, blocks['miss'])
