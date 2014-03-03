9e807f5f45f5968741b8d1e50f0c5826c20d7471
Mathias Bak <bufas7@gmail.com>
2014-03-03 18:10:46 +0100

    Upgrade linux/makeplot.sh

--- h1/plot/linux/makeplot.sh
+++ h1/plot/linux/makeplot.sh
@@ -4,19 +4,27 @@ rm -rf res
 mkdir res
 mkdir res/plots
 
+# g++ -O3 -std=c++11 -o benchmark ../benchmark_skew.cpp
+# ./benchmark 5 95 1000 100000 100 5 > res/dfs_explicit_int.1k.skew.bench.data
+# ./benchmark 5 95 10000 100000 100 5 > res/dfs_explicit_int.10k.skew.bench.data
+# ./benchmark 5 95 100000 100000 100 5 > res/dfs_explicit_int.100k.skew.bench.data
+# ./benchmark 5 95 1000000 100000 100 5 > res/dfs_explicit_int.1m.skew.bench.data
+# ./benchmark 5 95 10000000 100000 100 5 > res/dfs_explicit_int.10m.skew.bench.data
+# ./benchmark 5 95 100000000 100000 100 5 > res/dfs_explicit_int.100m.skew.bench.data
+
+
 g++ -O3 -std=c++11 -o benchmark ../benchmark.cpp
 
+layouts="inorder bfs dfs veb_explicit_int"
 perf_vars="BPU LL_CACHE L1_CACHE DATA_TLB"
-layouts="bfs dfs_explicit_int bfs_explicit_int veb_explicit_int"
 
-for perf in ${perf_vars}
+for layout in ${layouts}
 do
-    for layout in ${layouts}
+    for perf in ${perf_vars}
     do
-        ./benchmark ${layout} ${perf} 10 21 100000 100 5 > res/${layout}.${perf}.bench.data
+        ./benchmark ${layout} ${perf} 26 30 100000 100 5 0 1337 > res/${layout}.${perf}.bench.data
     done
 done
 
 rm benchmark
-gnuplot ../makeplot.plot
 
