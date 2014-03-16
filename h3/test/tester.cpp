959819896e5f94746b7c1b508735317025e5480d
Bufas <bufas7@gmail.com>
2014-03-16 04:14:16 +0100

    Implement indexed rank select
    Rank i O(1) while select is O(N). Space is O(N).

--- h3/test/tester.cpp
+++ h3/test/tester.cpp
@@ -1,14 +1,37 @@
 #include <boost/test/minimal.hpp>
+#include <vector>
+#include <iostream>
 #include "../include/RSNaive.h"
+#include "../include/RSIndexed.h"
+
+//____________________________________________________________________________//
+
+void test_rs_naive() {
+    RSNaive<10> rs("1010001101");
+    BOOST_CHECK( rs.rank(3) == 2 );
+    BOOST_CHECK( rs.select(5) == 10 );
+    BOOST_CHECK( rs.select(6) == -1 );
+}
+
+void test_rs_indexed() {
+    std::vector<unsigned long> v;
+    v.push_back(7);
+    v.push_back(1);
+    v.push_back(63);
+    RSIndexed<192> rs(v);
+
+    BOOST_CHECK( rs.rank(40) == 3 );
+    BOOST_CHECK( rs.rank(100) == 4 );
+    BOOST_CHECK( rs.select(5) == 129 );
+    BOOST_CHECK( rs.select(1) == 1 );
+}
 
 //____________________________________________________________________________//
 
 int test_main( int, char *[] )
 {
-    RSNaive<10> rs("1010001101");
-    BOOST_CHECK( rs.rank(3) == 2 );
-    BOOST_CHECK( rs.select(5) == 10 );
-    BOOST_CHECK( rs.select(6) == -1 );
+    test_rs_naive();
+    test_rs_indexed();
     return 0;
 }
 
